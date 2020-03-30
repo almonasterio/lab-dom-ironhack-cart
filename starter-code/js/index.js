53c92888fa7e7f65d6f0b726eb6e3182a11a75e3
almonasterio <a.monasterio.a@gmail.com>
2020-03-30 15:47:44 -0500

    lab done for practice

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,50 +1,82 @@
-let $cart = document.querySelector('#cart tbody');
-let $calc = document.getElementById('calc');
-let $deleteButtons =document.querySelectorAll(`.btn-delete`);
-let $erase =document.getElementById(`erase`);
+// ITERATION 1
 
-function updateSubtot($product) {
-  // Iteration 1.1
-  let priceUnit = $product.querySelector(`.pu span`).innerHTML;
-  let quantityProduct = $product.querySelector(`.qty label input`).value;
-  let subTotalSpan =$product.querySelector(`.subtot span`);
-  let subTotal =priceUnit*quantityProduct;
-  subTotalSpan.innerHTML = subTotal;
-  return  subTotal;
-}
+function updateSubtotal(product) {
+  for (product of product) {
+    const price = +product.querySelector('.price span').innerHTML;
+    const quantity = +product.querySelector('.quantity input').value;
+    let subtotal = price * quantity
+    product.querySelector('.subtotal span').innerHTML = subtotal
 
- // Iteration 1.2
-function calcAll() {
-  let totalSum=0;
-  let allItems =document.querySelectorAll( `.product`);
-  
-    allItems.forEach((product) => {
-    totalSum +=updateSubtot(product);
-  })
-  return document.querySelector(`h2 span`).innerHTML =totalSum; 
-}
-function deleteRow(elem){
-  elem = document.querySelector(`.product`);
-  elem.parentNode.removeChild(elem);
-}
-function deleteProduct() {
-  for (let i=0;i<$deleteButtons.length; i++){
-    if ($deleteButtons[i].onclick) {
-      //let rowToDelete =document.getElementsByClassName(`product`)[i];
-      deleteRow();
-    } 
   }
-  // let rowToRemove = document.querySelector('product');
-  // rowToRemove.parentNode.removeChild(rowToRemove);
+
+
 }
 
-//$deleteButtons.on('click',deleteRow); 
-$calc.onclick = calcAll;
-//e.currentTarget?
-$erase.onclick = deleteRow;
-//$deleteButtons.on('click', deleteRow);
-// let table = document.querySelector('.table')
-// table.childNodes.forEach(row => updateSubtot(row))
+function calculateAll() {
+  // code in the following two lines is added just for testing purposes.
+  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // end of test
 
-// for (var b = 0; b < button2.length; b++) {
-//   button1[b].addEventListener('click',function(){
+  // ITERATION 2
+  //... your code goes here
+  const productArr = document.getElementsByClassName("product")
+  updateSubtotal(productArr)
+  // ITERATION 3
+  const pricesArr = document.querySelectorAll(".subtotal span")
+  let totalPrice = 0
+  for (price of pricesArr) {
+    totalPrice += +price.innerHTML
+  };
+  console.log(totalPrice)
+  let totalEl = document.querySelector("#total-value span")
+  totalEl.innerText = totalPrice
+}
+
+
+// ITERATION 4
+
+function removeProduct(event) {
+  const target = event.currentTarget;
+
+  targetProductRow = target.parentNode.parentNode
+  targetProductRow.parentElement.removeChild(targetProductRow)
+  console.log('The target in remove is:', targetProductRow);
+  calculateAll()
+
+
+  //... your code goes here
+}
+
+// ITERATION 5
+
+function createProduct() {
+  //... your code goes here
+  const productParent = document.querySelector('.product').parentElement
+  const newProductName = document.querySelector(".create-product .new-name").value;
+  const newProductPrice = document.querySelector(".create-product .new-price").value;
+  const newProductRow = productParent.insertRow(document.querySelectorAll('.product').length);
+  newProductRow.classList.add("product")
+
+  newProductRow.innerHTML = `<td class="name"><span>${newProductName}</span></td><td class="price">$<span>${newProductPrice}</span></td><td class="quantity"><input type="number" value="10" min="0" placeholder="Quantity" /></td><td class="subtotal">$<span>1</span></td><td class="action"><button class="btn btn-remove">Remove</button></td>`;
+  newProductRow.lastChild.lastChild.addEventListener('click', removeProduct)
+
+  console.log(newProductRow.lastChild)
+
+  calculateAll()
+}
+
+window.addEventListener('load', () => {
+  calculateAll()
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+
+  //... your code goes here
+  const deleteButtonsArr = document.querySelectorAll('.btn-remove');
+  deleteButtonsArr.forEach(deleteBtn => deleteBtn.addEventListener('click', removeProduct))
+
+  const createButton = document.getElementById("create")
+  createButton.addEventListener('click', createProduct)
+
+});
